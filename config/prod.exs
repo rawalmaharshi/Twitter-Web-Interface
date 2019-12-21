71dc52eb2d2f23aad3a037fe5873dbe9f8cb13d4
Maharshi Rawal <rawalmaharshi@gmail.com>
2019-12-20 20:39:24 -0800

    changes for deployment 2

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :twitterPhx, TwitterPhxWeb.Endpoint,
   url: [scheme: "https", host: "twitter-engine-web.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 # Do not print debug messages in production
 config :logger, level: :info
